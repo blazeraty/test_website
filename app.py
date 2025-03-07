0b0450631fc82823715740825b330db1971df748
blazeraty <162861358+blazeraty@users.noreply.github.com>
2025-03-07 05:13:01 +0000

    Hello Teetys

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Teetys'
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
