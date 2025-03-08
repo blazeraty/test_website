f5bb775d54a403f327a779aa4cc08c3210cc2fb9
blazeraty <162861358+blazeraty@users.noreply.github.com>
2025-03-08 05:34:33 +0000

    added banner

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Teetys'
+    return render_template('home.html')
   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
